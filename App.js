cb4063f47b88000a4455a0c20cb8d4e275e376c9
benedictchuajj <benedictchuajj@gmail.com>
2021-05-16 20:13:44 +0800

    Navigation Bar consisting of { Home, Forum, Friends, Profile}

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons, FontAwesome5 } from 'react-native-vector-icons';
 
-export default function App() {
+import { HomeScreen, ForumScreen, FriendsScreen, ProfileScreen } from './src/screens';
+
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#aaf0d1"
+      labelStyle={{ fontSize: 12 }}
+      barStyle={{ backgroundColor: '#00203f' }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={28} style={{marginTop: -2, marginRight:1, width: 28}} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Forum"
+        component={ForumScreen}
+        options={{
+          tabBarLabel: 'Forum',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="forum" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Friends"
+        component={FriendsScreen}
+        options={{
+          tabBarLabel: 'Friends',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="user-friends" color={color} size={24} style={{width: 34}} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="user" color={color} size={24} solid />
+          ),
+        }}
+    />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/index.js
@@ -0,0 +1,4 @@
+export { default as HomeScreen } from './HomeScreen'
+export { default as ForumScreen } from './ForumScreen'
+export { default as FriendsScreen } from './FriendsScreen'
+export { default as ProfileScreen } from './ProfileScreen'
