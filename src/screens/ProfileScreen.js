c998675cfd6680f77660599822bdaaa4eaf6be3d
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-05-17 11:40:37 +0800

    profile page w comments and forum api pulling, personal not done yet

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,34 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import {
+  HomeScreen,
+  ProfileForumScreen,
+  ProfileCommentsScreen
+} from './'
+
+const Tab = createMaterialTopTabNavigator();
+
+function MyTabs() {
+  return (
+    <SafeAreaView style={styles.safe}>
+    <Tab.Navigator>
+      <Tab.Screen name="Personal" component={HomeScreen} />
+      <Tab.Screen name="Posts" component={ProfileForumScreen} />
+      <Tab.Screen name="Comments" component={ProfileCommentsScreen} />
+    </Tab.Navigator>
+    </SafeAreaView>
+  );
+}
 
 export default function ProfileScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile!</Text>
-    </View>
+    <MyTabs />
   );
 }
+
+const styles = StyleSheet.create({
+  safe: {
+    flex:1,
+  },
+})
