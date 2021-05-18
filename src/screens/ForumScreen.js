65ed0ad31e54ed86c23e374eeefc9ceb02006d2c
benedictchuajj <benedictchuajj@gmail.com>
2021-05-18 12:23:16 +0800

    Basic Forum Page with tab capabilities and search bar

--- src/screens/ForumScreen.js
+++ src/screens/ForumScreen.js
@@ -1,10 +1,38 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import SearchBar from '../components/SearchBar'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import {
+  ForumFavouritesScreen,
+  ForumSubscribedScreen,
+  ForumOthersScreen
+} from './'
+
+const TopTab = createMaterialTopTabNavigator();
+
+function MyTabs() {
+  return (
+    <TopTab.Navigator>
+      <TopTab.Screen name="Favourites" component={ForumFavouritesScreen} />
+      <TopTab.Screen name="Subscribed" component={ForumSubscribedScreen} />
+      <TopTab.Screen name="Others" component={ForumOthersScreen} />
+    </TopTab.Navigator>
+
+  );
+}
 
 export default function ForumScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Forum!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+        <SearchBar />
+        <MyTabs />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+});
