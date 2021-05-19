e244b06d2c6f3a2feccfba1d938a66b696c4e291
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-05-19 14:57:21 +0800

    increase font size, clean up

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Text, View, SafeAreaView, StyleSheet, StatusBar } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-//import { useNavigation } from '@react-navigation/native';
 import {
   ProfileMasterScreen,
   ProfileForumScreen,
