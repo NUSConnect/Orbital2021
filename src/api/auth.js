0bdc576b4df512005392cbe0fb06f7bcc274e7bb
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-05-21 11:45:29 +0800

    wip profile photo upload

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,4 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
+import * as firebase from 'firebase';
 
 export const logoutUser = () => {
   firebase.auth().signOut()
@@ -10,6 +9,22 @@ export const signUpUser = async ({ name, email, password }) => {
     const user = await firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        //Once the user creation has happened successfully, we can add the currentUser into firestore
+        //with the appropriate details.
+        firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid)
+        .set({
+            name: name,
+            email: email,
+            createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
+            userImg: null,
+        })
+        //ensure we catch any errors at this stage to advise us if something does go wrong
+        .catch(error => {
+            console.log('Something went wrong with added user to firestore: ', error);
+        })
+      })
+
     firebase.auth().currentUser.updateProfile({
       displayName: name,
     })
--- src/screens/profile/ProfilePersonalScreen.js
+++ src/screens/profile/ProfilePersonalScreen.js
@@ -3,29 +3,81 @@ import { TouchableOpacity, StyleSheet,Text,View, Image, SafeAreaView } from 'rea
 import { logoutUser } from '../../api/auth'
 import Button from '../../components/Button';
 import { theme } from '../../core/theme';
-import firebase from 'firebase/app';
+import * as firebase from 'firebase';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AccountSettingsScreen from './AccountSettingsScreen';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function ProfilePersonalScreen({navigation}) {
+export default class ProfilePersonalScreen extends React.Component {
+    state = {
+            imagePath: require("../../assets/placeholder.png"),
+            uploaded: false,
+            status: '',
+            imageURL: ''
+        }
+
+    static navigationOptions = {
+        header: null,
+      };
+
+      onChooseImagePress = async () => {
+        //let result = await ImagePicker.launchCameraAsync();
+        let result = await ImagePicker.launchImageLibraryAsync();
+
+        if (!result.cancelled) {
+          this.uploadImage(result.uri, firebase.auth().currentUser.uid)
+            .then(() => {
+              console.log("Success");
+              this.setState()
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      }
+
+      uploadImage = async (uri, imageName) => {
+        const response = await fetch(uri);
+        const blob = await response.blob();
+
+        var ref = firebase.storage().ref().child("profile/" + imageName);
+        this.setState({ imagePath:"profile/" + imageName });
+        console.log(this.state.imagePath);
+        firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).update({ userImg:this.state.imagePath })
+        return ref.put(blob);
+      }
+
+      getURI(imagePath) {
+          let imgSource = imagePath;
+          if (isNaN(imagePath)) {
+             imgSource = firebase.storage().ref().child(imagePath).getDownloadURL();
+          }
+          return imgSource;
+      }
+
+    render() {
+    let { imagePath } = this.state;
+    let imgSource = this.getURI(imagePath);
+    console.log(imgSource);
     return (
     <SafeAreaView>
 
       <View style={styles.container}>
          <View style={styles.header}>
             <View style={styles.headerContent}>
-                <Image style={styles.avatar}
-                  source={require('../../assets/logo.png')}/>
-
-                <Text style={styles.name}>{firebase.auth().currentUser.displayName}</Text>
-                <Text style={styles.userInfo}>{firebase.auth().currentUser.email}</Text>
-            </View>
+            <Image source={imgSource} />
+            <Button title="Choose image..." onPress={this.onChooseImagePress}>
+                Upload profile picture
+            </Button>
+            <Text style={styles.name}>{firebase.auth().currentUser.displayName}</Text>
+            <Text style={styles.userInfo}>{firebase.auth().currentUser.email}</Text>
          </View>
+      </View>
 
          <View style={styles.body}>
             <Button style = {styles.accountset}
-            mode="outlined" onPress={() => navigation.navigate('AccountSettingsScreen')} >
+            mode="outlined" onPress={() => this.props.navigation.navigate('AccountSettingsScreen')} >
                 Account Settings
             </Button>
 
@@ -37,6 +89,7 @@ export default function ProfilePersonalScreen({navigation}) {
       </View>
       </SafeAreaView>
     );
+    }
 }
 
 const styles = StyleSheet.create({
