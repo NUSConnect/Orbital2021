e66eedbb50a23b758b8d3141e6a748237732f335
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-05-24 15:38:03 +0800

    basic searchbar functionality with firebase user integration. styling to work on

--- src/screens/friends/FriendsScreen.js
+++ src/screens/friends/FriendsScreen.js
@@ -1,101 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, Text, StyleSheet, View, FlatList, StatusBar } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import React, { useState } from 'react'
+import { View, Text, TextInput, FlatList, TouchableOpacity, SafeAreaView, StatusBar, StyleSheet } from 'react-native'
+import SearchBar from '../../components/SearchBar';
+import firebase from 'firebase';
+require('firebase/firestore');
 
-export default function FriendsScreen() {
-  const [search, setSearch] = useState('');
-  const [filteredDataSource, setFilteredDataSource] = useState([]);
-  const [masterDataSource, setMasterDataSource] = useState([]);
+export default function FriendsScreen(props) {
+    const [users, setUsers] = useState([])
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        setFilteredDataSource(responseJson);
-        setMasterDataSource(responseJson);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-/*
-  const searchFilterFunction = (text) => {
-    // Check if searched text is not blank
-    if (text) {
-      const newData = masterDataSource.filter(function (item) {
-        const itemData = item.title
-          ? item.title.toUpperCase()
-          : ''.toUpperCase();
-        const textData = text.toUpperCase();
-        return itemData.indexOf(textData) > -1;
-      });
-      setFilteredDataSource(newData);
-      setSearch(text);
-    } else {
-      setFilteredDataSource(masterDataSource);
-      setSearch(text);
+    const fetchUsers = (search) => {
+        firebase.firestore()
+            .collection('users')
+            .where('name', '>=', search)
+            .get()
+            .then((snapshot) => {
+                let users = snapshot.docs.map(doc => {
+                    const data = doc.data();
+                    const id = doc.id;
+                    return { id, ...data }
+                });
+                setUsers(users);
+            })
     }
-  };
-*/
 
-  const ItemView = ({ item }) => {
+    const ItemView = ({ item }) => {
+        return (
+          <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+            {item.id}
+            {'.'}
+            {item.title.toUpperCase()}
+          </Text>
+        );
+      };
+
+      const ItemSeparatorView = () => {
+        return (
+          <View
+            style={{
+              height: 0.5,
+              width: '100%',
+              backgroundColor: '#C8C8C8',
+            }}
+          />
+        );
+      };
+
     return (
-      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
-        {item.id}
-        {'.'}
-        {item.title.toUpperCase()}
-      </Text>
-    );
-  };
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight || 0 }}>
+        <View>
+            <TextInput
+                placeholder="Type Here..."
+                onChangeText={(search) => fetchUsers(search)} />
 
-  const ItemSeparatorView = () => {
-    return (
-      <View
-        style={{
-          height: 0.5,
-          width: '100%',
-          backgroundColor: '#C8C8C8',
-        }}
-      />
-    );
-  };
-
-  const getItem = (item) => {
-    //action when item is clicked, leave as alert for now
-    alert('Id : ' + item.id + ' Title : ' + item.title);
-  };
-
-  return (
-    <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight || 0 }}>
-      <View style={styles.container}>
-        <SearchBar
-        placeholder="Type Here..."
-        /*
-          inputStyle={{backgroundColor: 'black'}}
-          inputContainerStyle={{backgroundColor: 'white'}}
-          containerStyle={{backgroundColor: 'white', borderWidth: 1, borderRadius: 5}}
-          searchIcon={{ size: 24 }}
-          onChangeText={(text) => searchFilterFunction(text)}
-          onClear={(text) => searchFilterFunction('')}
-          value={search}
-          inputStyle={{height:40}}
-          */
-        />
-
-        <View style={styles.flatListWindow}>
             <FlatList
-                data={filteredDataSource}
-                keyExtractor={(item, index) => index.toString()}
+                numColumns={1}
+                horizontal={false}
+                data={users}
                 ItemSeparatorComponent={ItemSeparatorView}
-                renderItem={ItemView}
+                renderItem={({ item }) => (
+                  <TouchableOpacity
+                    onPress={() => props.navigation.navigate("Profile", {uid: item.id})}>
+                  <Text>{item.name}</Text>
+                  </TouchableOpacity>
+                )}
             />
         </View>
-
-      </View>
-    </SafeAreaView>
-  );
-};
+      </SafeAreaView>
+    )
+}
 
 const styles = StyleSheet.create({
   container: {
