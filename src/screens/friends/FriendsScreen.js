b18a10f0ba2d6cd4546f9aab26152aa986e65e0e
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-05-24 20:55:05 +0800

    refined search functionality, reworked FriendsScreen.js UI. May want to further refine search down the road

--- src/screens/friends/FriendsScreen.js
+++ src/screens/friends/FriendsScreen.js
@@ -1,45 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, Text, StyleSheet, View, FlatList, StatusBar,ActivityIndicator } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  View,
+  FlatList,
+  TextInput,
+  ActivityIndicator
+} from 'react-native';
 import * as firebase from 'firebase';
 
 export default function FriendsScreen() {
-  const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState('');
   const [filteredDataSource, setFilteredDataSource] = useState([]);
   const [masterDataSource, setMasterDataSource] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [filtered, setFiltered] = useState(false);
 
   useEffect(() => {
-      const subscriber = firebase.firestore()
-        .collection('users')
-        .onSnapshot(querySnapshot => {
-          const users = [];
-
-          querySnapshot.forEach(documentSnapshot => {
-          console.log(documentSnapshot.data().name);
-          users.push(documentSnapshot.data().name)
+        const subscriber = firebase.firestore()
+          .collection('users')
+          .onSnapshot(querySnapshot => {
+            const users = [];
+            querySnapshot.forEach(documentSnapshot => {
+            console.log(documentSnapshot.data());
+            users.push(documentSnapshot.data())
+            });
+            console.log(users)
+            setMasterDataSource(users);
+            setLoading(false);
           });
-          console.log(users)
-          setMasterDataSource(users);
-          setLoading(false);
-        });
-      return () => subscriber();
-    }, []);
+        return () => subscriber();
+      }, []);
 
-    if (loading) {
-      return <ActivityIndicator />;
-    }
+      if (loading) {
+        return <ActivityIndicator />;
+      }
 
   const searchFilterFunction = (text) => {
-    // Check if searched text is not blank
     if (text) {
-      const newData = masterDataSource.filter(function (item) {
-        const itemData = item.title
-          ? item.title.toUpperCase()
-          : ''.toUpperCase();
-        const textData = text.toUpperCase();
-        return itemData.indexOf(textData) > -1;
+      const newData = masterDataSource.filter(
+        function (item) {
+          const itemData = item.name
+            ? item.name.toUpperCase()
+            : ''.toUpperCase();
+          const textData = text.toUpperCase();
+          return itemData.indexOf(textData) > -1;
       });
+      setFiltered(true);
       setFilteredDataSource(newData);
       setSearch(text);
     } else {
@@ -48,17 +56,20 @@ export default function FriendsScreen() {
     }
   };
 
-
-  const ItemView = ({ item }) => {
+  const ItemView = ({item}) => {
     return (
-      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
-        {item}
+      // Flat List Item
+      <Text
+        style={styles.itemStyle}
+        onPress={() => getItem(item)}>
+        {item.name}
       </Text>
     );
   };
 
   const ItemSeparatorView = () => {
     return (
+      // Flat List Item Separator
       <View
         style={{
           height: 0.5,
@@ -70,39 +81,25 @@ export default function FriendsScreen() {
   };
 
   const getItem = (item) => {
-    //action when item is clicked, leave as alert for now
-    alert('Name: ' + item);
+    // Function for click on an item
+    alert('Name : ' + item.name);
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight || 0 }}>
+    <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
-        <SearchBar
-        placeholder="Type Here..."
-        onChangeText={(text) => searchFilterFunction(text)}
-                  value={search}
-                  underlineColorAndroid="transparent"
-        /*
-          inputStyle={{backgroundColor: 'black'}}
-          inputContainerStyle={{backgroundColor: 'white'}}
-          containerStyle={{backgroundColor: 'white', borderWidth: 1, borderRadius: 5}}
-          searchIcon={{ size: 24 }}
+        <TextInput
+          style={styles.textInputStyle}
           onChangeText={(text) => searchFilterFunction(text)}
-          onClear={(text) => searchFilterFunction('')}
           value={search}
-          inputStyle={{height:40}}
-          */
+          placeholder="Search Here"
         />
-
-        <View style={styles.flatListWindow}>
-            <FlatList
-                data={masterDataSource}
-                keyExtractor={(item, index) => index.toString()}
-                ItemSeparatorComponent={ItemSeparatorView}
-                renderItem={ItemView}
-            />
-        </View>
-
+        <FlatList
+          data={filtered ? filteredDataSource : masterDataSource}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorView}
+          renderItem={ItemView}
+        />
       </View>
     </SafeAreaView>
   );
@@ -115,8 +112,12 @@ const styles = StyleSheet.create({
   itemStyle: {
     padding: 10,
   },
-  flatListWindow: {
-  //done via trial and error, check
-    paddingBottom:140,
+  textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 5,
+    borderColor: '#ff8c00',
+    backgroundColor: '#FFFFFF',
   },
 });
