fcc42fee822bc800607e913a84b57dd02792509d
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-05-26 21:06:44 +0800

    revert to previous commit

--- src/screens/home/ChatScreen.js
+++ src/screens/home/ChatScreen.js
@@ -1,97 +1,102 @@
-import React from "react";
-import {View, Text , StyleSheet, Platform, KeyboardAvoidingView, SafeAreaView } from 'react-native';
-import { GiftedChat, Message, Send } from 'react-native-gifted-chat';
+import React, {useState, useEffect, useCallback} from 'react';
+import {View, ScrollView, Text, Button, StyleSheet} from 'react-native';
+import {Bubble, GiftedChat, Send} from 'react-native-gifted-chat';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import Fire from "../Database";
 
-export default class ChatScreen extends React.Component{
+const ChatScreen = () => {
+  const [messages, setMessages] = useState([]);
 
-    state = {
-        messages: []
-    }
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+        },
+      },
+      {
+        _id: 2,
+        text: 'Hello',
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: 'React Native',
+        },
+      },
+    ]);
+  }, []);
 
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages),
+    );
+  }, []);
 
-    //this function is used to obtain the user detailes to the chat
-    get user(){
-        return{
-            _id: Fire.uid,
-            name: this.props.route.params.name
-        }
-    }
-
-
-    // here we obtain the data from the fire base db and apend it to the imported gited chat body
-    componentDidMount(){
-        Fire.get(message =>
-            this.setState(previous =>({
-            messages: GiftedChat.append(previous.messages,message)
-        })))
-    }
-
-    componentWillUnmount(){
-        Fire.off();
-    }
-
-     onSend = (messages = []) => {
-        this.setState( { messages: (previousMessages) =>
-          GiftedChat.append(previousMessages, messages)}
-        );
-      }
-
-     renderSend = (props) => {
-        return (
-          <Send {...props}>
-            <View>
-              <MaterialCommunityIcons
-                name="send-circle"
-                style={{marginBottom: 5, marginRight: 5}}
-                size={32}
-                color="#2e64e5"
-              />
-            </View>
-          </Send>
-        );
-      };
-
-     renderBubble = (props) => {
-        return (
-          <Bubble
-            {...props}
-            wrapperStyle={{
-              right: {
-                backgroundColor: '#2e64e5',
-              },
-            }}
-            textStyle={{
-              right: {
-                color: '#fff',
-              },
-            }}
+  const renderSend = (props) => {
+    return (
+      <Send {...props}>
+        <View>
+          <MaterialCommunityIcons
+            name="send-circle"
+            style={{marginBottom: 5, marginRight: 5}}
+            size={32}
+            color="#2e64e5"
           />
-        );
-      };
+        </View>
+      </Send>
+    );
+  };
 
-     scrollToBottomComponent = () => {
-        return(
-          <FontAwesome name='angle-double-down' size={22} color='#333' />
-        );
-      }
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#2e64e5',
+          },
+        }}
+        textStyle={{
+          right: {
+            color: '#fff',
+          },
+        }}
+      />
+    );
+  };
 
-    render(){
-        return (
-                <GiftedChat
-                  messages={this.state.messages}
-                  onSend={(message) => this.onSend(message)}
-                  user={{
-                    _id: 1,
-                  }}
-                  renderBubble={this.renderBubble}
-                  alwaysShowSend
-                  renderSend={this.renderSend}
-                  scrollToBottom
-                  scrollToBottomComponent={this.scrollToBottomComponent}
-                />
-              );
-    }
-}
+  const scrollToBottomComponent = () => {
+    return(
+      <FontAwesome name='angle-double-down' size={22} color='#333' />
+    );
+  }
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+      renderBubble={renderBubble}
+      alwaysShowSend
+      renderSend={renderSend}
+      scrollToBottom
+      scrollToBottomComponent={scrollToBottomComponent}
+    />
+  );
+};
+
+export default ChatScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
