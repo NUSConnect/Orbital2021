0023e1df93602a07b849ef0f734dbc57687964f0
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-06-03 11:46:12 +0800

    scoring algorithm drafts for posts. tests on dummyscreen

--- src/screens/profile/DummyScreen.js
+++ src/screens/profile/DummyScreen.js
@@ -1,8 +1,13 @@
 import * as React from "react";
 import { Text, View } from "react-native";
 import BackButton from "../../components/BackButton";
+import { trendingScore, newScore } from '../../api/ranking';
 
 export default function DummyScreen({ navigation }) {
+    console.log(trendingScore(100,100,40000));
+    console.log(trendingScore(100,90,40000));
+    //console.log(newScore(10));
+    //console.log(newScore(100));
     return (
         <View
             style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
