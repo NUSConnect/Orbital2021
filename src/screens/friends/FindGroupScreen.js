73e8e5b46bb10f620c78fe7d454487f00e821ec4
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-06-04 00:07:45 +0800

    create gridview for group options

--- src/screens/friends/FindGroupScreen.js
+++ src/screens/friends/FindGroupScreen.js
@@ -1,14 +1,46 @@
 import * as React from "react";
-import { Text, View } from "react-native";
-import BackButton from "../../components/BackButton";
+import { Text, View, Dimensions, StyleSheet } from "react-native";
+
+const DeviceWidth = Dimensions.get('window').width;
+const squareSide = 0.4 * DeviceWidth;
 
 export default function FindGroupScreen({ navigation }) {
+
     return (
-        <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-            <BackButton goBack={navigation.goBack} />
-            <Text>FindGroup</Text>
-        </View>
+        <View style={styles.center}>
+            <View style={{
+              flexDirection: 'row',
+              backgroundColor: "grey"}}>
+              <View>
+                <View style={styles.squareLeft} />
+                <View style={styles.squareRight} />
+              </View>
+              <View>
+                <View style={styles.squareLeft} />
+                <View style={styles.squareRight} />
+              </View>
+            </View>
+          </View>
     );
 }
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    squareLeft: {
+        width: squareSide,
+        height: squareSide,
+        marginBottom:1,
+        backgroundColor: 'powderblue',
+    },
+    squareRight: {
+        width: squareSide,
+        height: squareSide,
+        marginBottom:1,
+        marginLeft: 1,
+        backgroundColor: 'skyblue',
+    },
+});
