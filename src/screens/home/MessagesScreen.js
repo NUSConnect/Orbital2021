f2d33506b22dcc79df5e9f3d3e3aec5cf50ec7e4
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-06-12 11:48:48 +0800

    try implementing swipe to delete

--- src/screens/home/MessagesScreen.js
+++ src/screens/home/MessagesScreen.js
@@ -2,13 +2,17 @@ import * as firebase from "firebase";
 import moment from "moment";
 import React, { useEffect, useState } from "react";
 import {
+    Alert,
+    Animated,
     FlatList,
     StyleSheet,
     Text,
     TextInput,
-    View
+    TouchableOpacity,
+    View,
 } from "react-native";
 import { Divider } from "react-native-paper";
+import { Swipeable } from "react-native-gesture-handler";
 import MessageTopTab from "../../components/MessageTopTab";
 import {
     Card,
@@ -18,7 +22,7 @@ import {
     UserImgWrapper,
     UserInfo,
     UserInfoText,
-    UserName
+    UserName,
 } from "../../styles/MessageStyles";
 
 export default function MessagesScreen({ navigation }) {
@@ -31,12 +35,60 @@ export default function MessagesScreen({ navigation }) {
 
     useEffect(() => {
         getThreads();
-        const _unsubscribe = navigation.addListener('focus', () => getThreads());
+        const _unsubscribe = navigation.addListener("focus", () =>
+            getThreads()
+        );
 
         return () => {
             _unsubscribe();
-        }
+        };
     }, []);
+
+    const deletePressed = () => {
+        console.log("Pressed");
+        Alert.alert(
+            "Are you sure?",
+            "Chat will be deleted from your messages",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Cancel Pressed!"),
+                    style: "cancel",
+                },
+                {
+                    text: "Delete",
+                    onPress: () => {
+                        console.log("Delete");
+                        firebase
+                        .firestore()
+                        .collection("users")
+                        .doc(currentUserId)
+                        .collection("openChats")
+                        .doc("7lPhX8jHgl45")
+                        .delete();
+                    },
+                },
+            ],
+            { cancelable: false }
+        );
+    };
+
+    const rightSwipe = (progress, dragX) => {
+        const scale = dragX.interpolate({
+            inputRange: [0, 100],
+            outputRange: [1, 0],
+            extrapolate: "clamp",
+        });
+        return (
+            <TouchableOpacity onPress={deletePressed} activeOpacity={0.6}>
+                <View style={styles.deleteBox}>
+                    <Animated.Text style={{ transform: [{ scale: scale }] }}>
+                        Delete
+                    </Animated.Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
 
     const getUserInfo = async () => {
         await firebase
@@ -54,27 +106,36 @@ export default function MessagesScreen({ navigation }) {
 
     const matchUserToThreads = async (threads) => {
         for (let k = 0; k < threads.length; k++) {
-            const threadId = threads[k].id
+            const threadId = threads[k].id;
 
             var users;
             var isGroup;
-            await firebase.firestore().collection("THREADS").doc(threadId).get()
+            await firebase
+                .firestore()
+                .collection("THREADS")
+                .doc(threadId)
+                .get()
                 .then((doc) => {
-                    users = doc.data().users
-                    threads[k].latest = doc.data().latestMessage.createdAt
-                    threads[k].message = doc.data().latestMessage.text
-                    isGroup = doc.data().group
+                    users = doc.data().users;
+                    threads[k].latest = doc.data().latestMessage.createdAt;
+                    threads[k].message = doc.data().latestMessage.text;
+                    isGroup = doc.data().group;
                 });
 
             if (isGroup) {
-                await firebase.firestore().collection("THREADS").doc(threadId).get()
+                await firebase
+                    .firestore()
+                    .collection("THREADS")
+                    .doc(threadId)
+                    .get()
                     .then((doc) => {
                         threads[k].name = doc.data().groupName.name;
                         threads[k].avatar = doc.data().groupImage;
-                        threads[k].description = doc.data().groupDescription.description;
+                        threads[k].description =
+                            doc.data().groupDescription.description;
                         threads[k].members = doc.data().users;
                         threads[k].isGroup = true;
-                    })
+                    });
             } else {
                 for (let i = 0; i < users.length; i++) {
                     if (users[i] != currentUserId) {
@@ -87,7 +148,7 @@ export default function MessagesScreen({ navigation }) {
                                 if (doc.exists) {
                                     threads[k].name = doc.data().name;
                                     threads[k].avatar = doc.data().userImg;
-                                    threads[k].otherId = users[i]
+                                    threads[k].otherId = users[i];
                                 } else {
                                     threads[k].name = "anon";
                                     threads[k].avatar = null;
@@ -98,10 +159,10 @@ export default function MessagesScreen({ navigation }) {
             }
         }
         threads.sort((x, y) => {
-            return y.latest - x.latest
-        })
+            return y.latest - x.latest;
+        });
         setThreads(threads);
-//        console.log("Threads: ", threads);
+        //        console.log("Threads: ", threads);
     };
 
     const getThreads = async () => {
@@ -142,7 +203,10 @@ export default function MessagesScreen({ navigation }) {
 
     return (
         <View style={styles.container}>
-            <MessageTopTab onBack={() => navigation.goBack()} onPress={() => navigation.navigate('StartMessagesScreen')} />
+            <MessageTopTab
+                onBack={() => navigation.goBack()}
+                onPress={() => navigation.navigate("StartMessagesScreen")}
+            />
             <TextInput
                 style={styles.textInputStyle}
                 onChangeText={(text) => searchFilterFunction(text)}
@@ -154,28 +218,34 @@ export default function MessagesScreen({ navigation }) {
                 keyExtractor={(item) => item.name}
                 ItemSeparatorComponent={() => <Divider />}
                 renderItem={({ item }) => (
-                    <Card
-                        onPress={() =>
-                            navigation.navigate("ChatScreen", { thread: item })
-                        }
-                    >
-                        <UserInfo>
-                            <UserImgWrapper>
-                                <UserImg source={{ uri: item.avatar }} />
-                            </UserImgWrapper>
-                            <TextSection>
-                                <UserInfoText>
-                                    <UserName>{item.name}</UserName>
-                                    <PostTime>
-                                        {moment(item.latest).fromNow()}
-                                    </PostTime>
-                                </UserInfoText>
-                                <Text style={styles.text}>{item.message}</Text>
-                            </TextSection>
-                        </UserInfo>
-                    </Card>
+                    <Swipeable renderRightActions={rightSwipe}>
+                        <Card
+                            onPress={() =>
+                                navigation.navigate("ChatScreen", {
+                                    thread: item,
+                                })
+                            }
+                        >
+                            <UserInfo>
+                                <UserImgWrapper>
+                                    <UserImg source={{ uri: item.avatar }} />
+                                </UserImgWrapper>
+                                <TextSection>
+                                    <UserInfoText>
+                                        <UserName>{item.name}</UserName>
+                                        <PostTime>
+                                            {moment(item.latest).fromNow()}
+                                        </PostTime>
+                                    </UserInfoText>
+                                    <Text style={styles.text}>
+                                        {item.message}
+                                    </Text>
+                                </TextSection>
+                            </UserInfo>
+                        </Card>
+                    </Swipeable>
                 )}
-                style={{ marginBottom: 40, }}
+                style={{ marginBottom: 40 }}
             />
         </View>
     );
@@ -208,7 +278,14 @@ const styles = StyleSheet.create({
         color: "#ffffff",
     },
     text: {
-        color: 'black',
+        color: "black",
         fontSize: 16,
-    }
+    },
+    deleteBox: {
+        backgroundColor: "red",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 100,
+        height: 80,
+    },
 });
