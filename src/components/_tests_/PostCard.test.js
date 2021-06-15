c7d2052a2ce419cbeea5e9cd38c4b5b9f7274671
benedictchuajj <benedictchuajj@gmail.com>
2021-06-15 11:36:17 +0800

    testing children for forumIcon componennt

--- src/components/_tests_/PostCard.test.js
+++ src/components/_tests_/PostCard.test.js
@@ -1,8 +1,9 @@
 import * as React from "react";
 import PostCard from "../PostCard";
 import renderer from "react-test-renderer";
+import 'jest-styled-components'
 
 it(`renders correctly`, () => {
-    const tree = renderer.create(<PostCard />);
+    const tree = renderer.create(<PostCard />).toJSON();
     expect(tree).toMatchSnapshot();
 });
