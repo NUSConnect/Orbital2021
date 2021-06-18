618c64675fc193303ced7ad4624483116db03fcf
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-06-18 09:54:56 +0800

    add searchbar x icon for messages, enlarge friends search x icon

--- src/screens/friends/FriendSearchScreen.js
+++ src/screens/friends/FriendSearchScreen.js
@@ -159,9 +159,10 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: "flex-end",
         marginRight: 10,
+        marginBottom: 15,
     },
     closeButton: {
-        height: 16,
-        width: 16,
+        height: 20,
+        width: 20,
     },
 });
