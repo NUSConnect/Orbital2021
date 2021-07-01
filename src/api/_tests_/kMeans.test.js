2814576a32ad6219d9a8ee15a036d5ceca832781
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-07-01 22:02:57 +0800

    fixed kmeans tests

--- src/api/_tests_/kMeans.test.js
+++ src/api/_tests_/kMeans.test.js
@@ -5,7 +5,7 @@ it('defaultVectorFunc returns the vector correctly', () => {
 })
 
 it('getNumDimensions returns correct number of dimensions', () => {
-  expect(kMeans.getNumDimensions([[1, 2, 3], [4, 5, 6]], kMeans.defaultVectorFunc)).toBe(3)
+  expect(kMeans.getNumDimensions([{ vector: [1, 2, 3] }, { vector: [4, 5, 6] }], kMeans.defaultVectorFunc)).toBe(3)
 })
 
 it('getNumDimensions returns null when no data is provided', () => {
@@ -25,35 +25,35 @@ it('initMinMax initializes the min max values correctly', () => {
 })
 
 it('getMinMax adjusts min max values correctly', () => {
-  expect(kMeans.getMinMax([[-100000, -100000], [0, 0], [100000, 100000]], 2, kMeans.defaultVectorFunc))
+  expect(kMeans.getMinMax([{ vector: [-100000, -100000] }, { vector: [0, 0] }, { vector: [100000, 100000] }], 2, kMeans.defaultVectorFunc))
     .toEqual({ maxValue: [100000, 100000], minValue: [-100000, -100000] })
 })
 
 it('getMean calculates mean correctly', () => {
-  expect(kMeans.getMean([[-100000, -100000], [0, 0], [100000, 100000]], 0, kMeans.defaultVectorFunc)).toEqual(0)
-  expect(kMeans.getMean([[-100000, -100000], [0, 0], [100000, 100000]], 1, kMeans.defaultVectorFunc)).toEqual(0)
+  expect(kMeans.getMean([{ vector: [-100000, -100000] }, { vector: [0, 0] }, { vector: [100000, 100000] }], 0, kMeans.defaultVectorFunc)).toEqual(0)
+  expect(kMeans.getMean([{ vector: [-100000, -100000] }, { vector: [0, 0] }, { vector: [100000, 100000] }], 1, kMeans.defaultVectorFunc)).toEqual(0)
 })
 
 it('getMean calculates mean correctly', () => {
-  expect(kMeans.getMean([[-100000, -100000], [0, 0], [100000, 1000000]], 0, kMeans.defaultVectorFunc)).toEqual(0)
-  expect(kMeans.getMean([[-100000, -100000], [0, 0], [100000, 1000000]], 1, kMeans.defaultVectorFunc)).toEqual(300000)
+  expect(kMeans.getMean([{ vector: [-100000, -100000] }, { vector: [0, 0] }, { vector: [100000, 1000000] }], 0, kMeans.defaultVectorFunc)).toEqual(0)
+  expect(kMeans.getMean([{ vector: [-100000, -100000] }, { vector: [0, 0] }, { vector: [100000, 1000000] }], 1, kMeans.defaultVectorFunc)).toEqual(300000)
 })
 
 it('updateMean updates mean correctly', () => {
-  const cluster = { data: [[-100000, -100000], [0, 0], [100000, 100000]], mean: { length: 2 } }
+  const cluster = { data: [{ vector: [-100000, -100000] }, { vector: [0, 0] }, { vector: [100000, 100000] }], mean: { length: 2 } }
   kMeans.updateMean(cluster, kMeans.defaultVectorFunc)
   expect(cluster.mean).toEqual([0, 0])
 })
 
 it('updateMean updates mean correctly', () => {
-  const cluster = { data: [[-100000, -100000], [0, 0], [100000, 1000000]], mean: { length: 2 } }
+  const cluster = { data: [{ vector: [-100000, -100000] }, { vector: [0, 0] }, { vector: [100000, 1000000] }], mean: { length: 2 } }
   kMeans.updateMean(cluster, kMeans.defaultVectorFunc)
   expect(cluster.mean).toEqual([0, 300000])
 })
 
 it('updateMeans updates means correctly', () => {
-  const cluster1 = { data: [[-100000, -100000], [0, 0], [100000, 100000]], mean: { length: 2 } }
-  const cluster2 = { data: [[-100000, -100000], [0, 0], [100000, 1000000]], mean: { length: 2 } }
+  const cluster1 = { data: [{ vector: [-100000, -100000] }, { vector: [0, 0] }, { vector: [100000, 100000] }], mean: { length: 2 } }
+  const cluster2 = { data: [{ vector: [-100000, -100000] }, { vector: [0, 0] }, { vector: [100000, 1000000] }], mean: { length: 2 } }
   const clusters = [cluster1, cluster2]
   kMeans.updateMeans(clusters, kMeans.defaultVectorFunc)
   expect(cluster1.mean).toEqual([0, 0])
@@ -68,12 +68,12 @@ it('findClosestCluster finds the nearest cluster correctly', () => {
 })
 
 it('assignDataToClusters pushes data correctly', () => {
-  const cluster1 = { data: [[0, 0]], mean: [0, 0] }
-  const cluster2 = { data: [[100000, 1000000]], mean: [100000, 1000000] }
+  const cluster1 = { data: [{ vector: [0, 0] }], mean: [0, 0] }
+  const cluster2 = { data: [{ vector: [100000, 1000000] }], mean: [100000, 1000000] }
   const clusters = [cluster1, cluster2]
-  kMeans.assignDataToClusters([[1, 1], [100000, 1000001]], clusters, kMeans.getDistance, kMeans.defaultVectorFunc)
-  expect(cluster1.data).toEqual([[0, 0], [1, 1]])
-  expect(cluster2.data).toEqual([[100000, 1000000], [100000, 1000001]])
+  kMeans.assignDataToClusters([{ vector: [1, 1] }, { vector: [100000, 1000001] }], clusters, kMeans.getDistance, kMeans.defaultVectorFunc)
+  expect(cluster1.data).toEqual([{ vector: [0, 0] }, { vector: [1, 1] }])
+  expect(cluster2.data).toEqual([{ vector: [100000, 1000000] }, { vector: [100000, 1000001] }])
 })
 
 it('initClustersData initializes data correctly', () => {
@@ -111,7 +111,8 @@ it('getDistance returns the correct Euclidean distance', () => {
 })
 
 it('getClusters returns the correct info', () => {
-  const data = [[1, 2, 1, 2], [3, 4, 3, 4], [10, 11, 10, 11], [12, 13, 12, 13]]
-  expect(kMeans.getClusters(data, 2)).toEqual([{ data: [[10, 11, 10, 11], [12, 13, 12, 13]], mean: [11, 12, 11, 12] }, { data: [[1, 2, 1, 2], [3, 4, 3, 4]], mean: [2, 3, 2, 3] }]
+  const data = [{ vector: [1, 2, 1, 2] }, { vector: [3, 4, 3, 4] }, { vector: [10, 11, 10, 11] }, { vector: [12, 13, 12, 13] }]
+  expect(kMeans.getClusters(data, 2)).toEqual([{ data: [{ vector: [10, 11, 10, 11] },
+  { vector: [12, 13, 12, 13] }], mean: [11, 12, 11, 12] }, { data: [{ vector: [1, 2, 1, 2] }, { vector: [3, 4, 3, 4] }], mean: [2, 3, 2, 3] }]
   )
 })
