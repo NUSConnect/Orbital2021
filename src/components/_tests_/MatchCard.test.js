197535ae167637bcaa7588ffcb18f4622864791b
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-07-04 21:23:10 +0800

    comprehensive matchcard tests

--- src/components/_tests_/MatchCard.test.js
+++ src/components/_tests_/MatchCard.test.js
@@ -35,20 +35,45 @@ class CustomDate {
 const setDate = new Date(2021, 5, 15, 12, 0, 0)
 const timestamp = new CustomDate(setDate)
 
-const mockItem = {
+const mockItem1 = {
   success: true,
   timeMatched: timestamp,
   isGroup: true
 }
 
+const mockItem2 = {
+  success: false,
+  timeMatched: timestamp
+}
+
+const mockItem3 = {
+  success: true,
+  timeMatched: timestamp,
+  isGroup: false
+}
+
 it('renders correctly', () => {
-  const tree = renderer.create(<MatchCard item={mockItem} />)
+  const tree = renderer.create(<MatchCard item={mockItem1} />)
   expect(tree).toMatchSnapshot()
 })
 
-it('on press card', () => {
+it('on press card with group', () => {
   console.log = jest.fn()
-  const { getByTestId } = render(<MatchCard item={mockItem} />)
+  const { getByTestId } = render(<MatchCard item={mockItem1} />)
   fireEvent.press(getByTestId('pressable'))
-  expect(console.log).toHaveBeenCalledWith('group or fail')
+  expect(console.log).toHaveBeenCalledWith('Group')
+})
+
+it('on press card with failure', () => {
+  console.log = jest.fn()
+  const { getByTestId } = render(<MatchCard item={mockItem2} />)
+  fireEvent.press(getByTestId('pressable'))
+  expect(console.log).toHaveBeenCalledWith('Failure')
+})
+
+it('on press card with individual', () => {
+  console.log = jest.fn()
+  const { getByTestId } = render(<MatchCard item={mockItem3} />)
+  fireEvent.press(getByTestId('pressable'))
+  expect(console.log).toHaveBeenCalledWith('Individual, go to view profile')
 })
