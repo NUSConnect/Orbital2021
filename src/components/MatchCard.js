efd212e37054f4bd780b7a9ea648509ccd010136
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-07-04 10:54:27 +0800

    add timestamp to matchcards

--- src/components/MatchCard.js
+++ src/components/MatchCard.js
@@ -90,8 +90,8 @@ const MatchCard = ({ item, onPress }) => {
           <Text style={styles.title}> {item.success ? 'Success!' : 'Sorry!'} </Text>
           {item.success
             ? item.isGroup
-                ? <Text style={styles.info}> Match successful! Tap here to chat with your new group! </Text>
-                : <Text style={styles.info}> Match successful! Tap here to check out {otherName}{'\'s profile!'}</Text>
+                ? <Text style={styles.info}> Match on {item.timeMatched.toDate().toDateString()} successful! Tap here to chat with your new group! </Text>
+                : <Text style={styles.info}> Match on {item.timeMatched.toDate().toDateString()} successful! Tap here to check out {otherName}{'\'s profile!'}</Text>
             : <Text style={styles.info}> Match failed, better luck next time! </Text>}
         </Card>
       </TouchableOpacity>
