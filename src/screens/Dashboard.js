fbe39a54c87c5780d4bc20684b650f385ec8f494
Lua Yi Da <77190041+luayida99@users.noreply.github.com>
2021-07-21 22:01:38 +0800

    Update Dashboard.js

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react'
 import * as Permissions from 'expo-permissions'
 import * as Notifications from 'expo-notifications'
 import * as firebase from 'firebase'
-import checkIfFirstLaunch from '../api/firstLaunch'
 import { FontAwesome5, MaterialCommunityIcons, Ionicons } from 'react-native-vector-icons'
 import {
   ForumScreen,
