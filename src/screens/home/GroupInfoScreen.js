28261f58f2eb84562553f6a44fa0ed0305f77a84
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-07-24 10:52:00 +0800

    add default avatar to messages

--- src/screens/home/GroupInfoScreen.js
+++ src/screens/home/GroupInfoScreen.js
@@ -185,7 +185,7 @@ const GroupInfoScreen = ({ navigation, route, onPress }) => {
           >
             <UserInfo>
               <UserImgWrapper>
-                <UserImg source={{ uri: item.userImg }} />
+                <UserImg source={{ uri: item.userImg || 'https://firebasestorage.googleapis.com/v0/b/orbital2021-a4766.appspot.com/o/profile%2Fplaceholder.png?alt=media&token=52e7df63-abdf-4197-9ad3-79d4be61af10' }} />
               </UserImgWrapper>
               <TextSection>
                 <UserInfoText>
--- src/screens/home/MessagesScreen.js
+++ src/screens/home/MessagesScreen.js
@@ -338,7 +338,7 @@ export default function MessagesScreen ({ navigation }) {
                   >
                     <UserInfo>
                       <UserImgWrapper>
-                        <UserImg source={{ uri: item.avatar }} />
+                        <UserImg source={{ uri: item.avatar || 'https://firebasestorage.googleapis.com/v0/b/orbital2021-a4766.appspot.com/o/profile%2Fplaceholder.png?alt=media&token=52e7df63-abdf-4197-9ad3-79d4be61af10' }} />
                       </UserImgWrapper>
                       <TextSection>
                         <UserInfoText>
