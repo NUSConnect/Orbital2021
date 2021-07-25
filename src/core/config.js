3d4964fdd29115aee981525b51c60dfb6139431c
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-07-25 23:04:11 +0800

    fix inconsistent styling across forumpostheader and subforumheader

--- src/core/config.js
+++ src/core/config.js
@@ -1,9 +1,9 @@
 export const FIREBASE_CONFIG = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: ''
+  apiKey: 'AIzaSyB0HAlRGcUkjeklySpSjXTnlWKlh26rEwk',
+  authDomain: 'orbital2021-a4766.firebaseapp.com',
+  projectId: 'orbital2021-a4766',
+  storageBucket: 'orbital2021-a4766.appspot.com',
+  messagingSenderId: '323827934145',
+  appId: '1:323827934145:web:ff5bbb2bec368bb5580540',
+  measurementId: 'G-2ESR4KWTG4'
 }
